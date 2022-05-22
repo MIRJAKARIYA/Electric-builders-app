2b7b2aaa4c5610c043e56de1671e2e3ceb026ebc
MIRJAKARIYA <mirjakariya239@gmail.com>
2022-05-23 03:17:02 +0600

    tools added in the home page

--- src/Pages/Home/SingleTool.js
+++ src/Pages/Home/SingleTool.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SingleTool = ({ tool }) => {
-  console.log(tool);
+  const [showMore, setShowMore] = useState(false);
   const {
     availableQuantity,
     description,
@@ -12,19 +12,27 @@ const SingleTool = ({ tool }) => {
     _id,
   } = tool;
   return (
-    <div className="card card-compact p-4 bg-slate-700 text-white shadow-xl">
-      <figure>
-        <img
-          src={img}
-          alt="Shoes"
-          className="w-full h-[250px] object-cover rounded-2xl"
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{toolName}</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-warning mb-[-13px]">Buy Now</button>
+    <div>
+      <div
+        data-aos="fade-up"
+        data-aos-duration="500"
+        data-aos-easing="ease-in-out"
+        className="card rounded-xl p-3 bg-slate-700 text-white shadow-xl"
+      >
+        <figure>
+          <img
+            src={img}
+            alt="Shoes"
+            className="w-full h-[250px] object-cover rounded-xl"
+          />
+        </figure>
+        <div className="">
+          <h2 className="card-title">{toolName}</h2>
+          <p>{showMore ? description : description.slice(0, 100)}</p>
+          <button onClick={() => setShowMore(!showMore)}>{showMore?'Show less':'Show more'}</button>
+          <div className="card-actions justify-end">
+            <button className="btn btn-warning">Place oreder</button>
+          </div>
         </div>
       </div>
     </div>
--- src/Pages/Home/Tools.js
+++ src/Pages/Home/Tools.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import SingleTool from "./SingleTool";
+import { BsFillArrowDownCircleFill, BsFillArrowUpCircleFill } from 'react-icons/bs'
 
 const Tools = () => {
+    const [sliceCount, setSliceCount] = useState(6);
+    const handleShowMore = () =>{
+        setSliceCount(10)
+    }
+    const handleShowLess = () =>{
+        setSliceCount(6)
+    }
   const {
     isLoading,
     error,
@@ -15,12 +23,17 @@ const Tools = () => {
 
   return (
    <div className="max-w-[1200px] mx-auto mt-10">
-       <h1 className="text-center mb-8 text-2xl font-semibold text-red-700">Tools</h1>
-        <div className="grid grid-cols-3 justify-items-center gap-5">
+       <h1 className="text-center mb-5 text-2xl font-semibold text-red-700">Tools</h1>
+        <div className="grid mb-2 sm:grid-cols-2 md:grid-cols-3 justify-items-center gap-5">
         {
-            tools?.slice(0,6).map(tool => <SingleTool key={tool._id} tool={tool}></SingleTool>)
+            tools?.slice(0,sliceCount).map(tool => <SingleTool key={tool._id} tool={tool}></SingleTool>)
         }
     </div>
+    {
+        sliceCount === 6 ? <button className="text-blue-800 font-semibold mx-auto block" onClick={handleShowMore}><div className="flex items-center gap-1"><span>Show more</span><BsFillArrowDownCircleFill/></div></button>
+        :
+        <button className="text-blue-800 font-semibold mx-auto block" onClick={handleShowLess}><div className="flex items-center gap-1"><span>Show less</span><BsFillArrowUpCircleFill/></div></button>
+    }
    </div>
   );
 };
