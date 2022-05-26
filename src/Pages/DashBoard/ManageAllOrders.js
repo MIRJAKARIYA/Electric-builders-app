003cef8300e7e1e952570030934319e72311d8ea
MIRJAKARIYA <mirjakariya239@gmail.com>
2022-05-26 22:26:30 +0600

    simple changes

--- src/Pages/DashBoard/ManageAllOrders.js
+++ src/Pages/DashBoard/ManageAllOrders.js
@@ -1,6 +1,7 @@
 import { signOut } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import auth from "../../firebase.init";
 import DeleteOrderModal from "./DeleteOrderModal";
 import DeliverModal from "./DeliverModal";
@@ -13,8 +14,11 @@ const ManageAllOrders = () => {
     const [reversedOrders, setReversedOrders] = useState([]);
     const [deliverModal, setDeliverModal] = useState(null);
     const [deleteModal, setDeleteModal] = useState(null);   
+    const [query, setQuery] = useState('')
+
+
     useEffect(()=>{
-        fetch('http://localhost:5000/purchased',{
+        fetch(`http://localhost:5000/adminPurchased?${query}`,{
           method:'GET',
           headers:{
             authorization: `Bearer ${localStorage.getItem("ACCESS_TOKEN")}`,
@@ -32,19 +36,27 @@ const ManageAllOrders = () => {
         .then(data => {
             setOrders(data)
         })
-    },[reload,navigate])
+    },[reload,navigate,query])
     useEffect(()=>{
-        if(orders.length>0){
+
             setReversedOrders(orders.reverse())
-        }
+
+
     },[orders])
     
   return (
     <>
       <h1 className="text-center mt-5 text-2xl font-semibold text-red-700">Manage All Orders</h1>
+      <div className="text-center my-10">
+      <button className="btn btn-warning btn-sm mr-4" onClick={()=>setQuery('delivery=pending')}>Pending orders</button>
+      <button className="btn btn-success btn-sm mr-4" onClick={()=>setQuery('delivery=shipped')}>Shipped orders</button>
+      <button className="btn btn-error btn-sm mr-4" onClick={()=>setQuery('status=unpaid')}>Unpaid orders</button>
+      <button className="btn btn-accent btn-sm " onClick={()=>setQuery('')}>All orders</button>
+      </div>
       <div className="mr-auto lg:mr-0 ml-auto w-[95%] px-5 mt-5">
         <div className="overflow-x-auto">
-          <table className="table w-full">
+          {
+            reversedOrders?.length === 0 ? <h1 className="text-xl text-red-700 text-center font-semibold">No order found</h1>:<table className="table w-full">
             <thead>
               <tr>
                 <th></th>
@@ -65,6 +77,7 @@ const ManageAllOrders = () => {
                 }
             </tbody>
           </table>
+          }
         </div>
         {
             deliverModal && <DeliverModal reload={reload} setReload={setReload} deliverModal={deliverModal} setDeliverModal={setDeliverModal}></DeliverModal>
